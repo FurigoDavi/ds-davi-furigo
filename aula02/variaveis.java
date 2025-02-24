c637968d18a1def82c2ef05b6a7c098bd4c40ef4
Davi Furigo <davifurigo107@gmail.com>
2025-02-24 09:30:54 -0300

    Aula de java

---
+++ aula02/variaveis.java
@@ -0,0 +1,24 @@
+public class variaveis {
+    public static void main(String[] args) {
+
+        String name = "Cleber Murissoca";
+        System.out.println(name);
+
+        int idade = 16;
+        System.out.println(idade);
+
+        float altura = 1.7f;
+        System.out.println(altura);
+
+        float peso = 68.8f;
+        System.out.println(peso);
+
+        System.out.println(altura / idade + peso);
+
+        char inicial ='C';
+        System.out.println(inicial);
+
+        boolean sigma = true;
+        System.out.println(sigma);        
+    }
+}
