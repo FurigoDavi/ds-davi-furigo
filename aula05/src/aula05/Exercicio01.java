8451ed127b3182575db0ad587516cb127d086029
Davi Furigo <davifurigo107@gmail.com>
2025-04-28 09:19:11 -0300

    Netbeans

---
+++ aula05/src/aula05/Exercicio01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Exercicio01 {
+    
+     public static void main(String[] args){
+         
+         double gastosJaneiro = 15000;
+         
+         double gastosFevereiro = 23000;
+         
+         double gastosMarco = 17000;
+         
+         double gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+         
+         System.out.println("O gasto do trimestre é: " + gastosTrimestre);
+     }
+    
+}
