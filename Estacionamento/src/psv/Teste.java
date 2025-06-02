f02433c3d4568adc5aa6e7d03b49c3e42f079ea7
Davi Furigo <davifurigo107@gmail.com>
2025-06-02 09:25:35 -0300

    Calculadora

---
+++ Estacionamento/src/psv/Teste.java
@@ -0,0 +1,45 @@
+package psv;
+
+import com.sun.jdi.connect.spi.Connection;
+
+import java.sql.*; 
+
+import java.util.*; 
+
+public class Teste {
+     public static void main(String[] args) { 
+        var con = (Connection) Conexao.abrirConexao();
+        
+        CarroBean cb = new CarroBean(); 
+ 
+        CarroDAO cd = new CarroDAO(con); 
+ 
+        //Testando método inserir 
+        cb.setPlaca("JKL2897"); 
+        cb.setCor("Verde"); 
+        cb.setDescricao("Carro 3"); 
+ 
+        System.out.println(cd.inserir(cb)); 
+ 
+        //Testando método alterar 
+        cb.setPlaca("JKL2897"); 
+        cb.setCor("Amarelo"); 
+        cb.setDescricao("Carro 3"); 
+ 
+        System.out.println(cd.alterar(cb));
+ 
+        cb.setPlaca("JKL2897"); 
+        System.out.println(cd.excluir(cb));
+
+        List<CarroBean> lista = cd.listarTodos(); 
+ 
+        if(lista != null){ 
+            for(CarroBean carro : lista){ 
+                System.out.println("Placa: "+carro.getPlaca()); 
+                System.out.println("Cor: "+carro.getCor()); 
+                System.out.println("Descrição: "+carro.getDescricao()); 
+            } 
+        } 
+        Conexao.fecharConexao(con);
+     }
+}
