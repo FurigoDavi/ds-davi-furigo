f4a4cfe3997ff4a6fce7c910420d06865c6dcdad
Davi Furigo <davifurigo107@gmail.com>
2025-08-04 09:26:41 -0300

    Aula Usuario

---
+++ aula201/exercicio01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args){
+
+        private String nome;
+        private String email;
+        private int idade;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira seu nome: ");
+        String nome = scan.nextLine();
+
+    }
+}
